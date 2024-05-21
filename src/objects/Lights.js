df33dc97710a9c177bd46e7c14cb1b2108e94c0b
Kartoz9 <gluhovski241@gmail.com>
2024-05-21 15:39:35 +0300

    Orange

--- src/objects/Lights.js
+++ src/objects/Lights.js
@@ -4,10 +4,11 @@ export default class BasicLights extends Group {
   constructor(...args) {
     super(...args);
 
-    const point = new PointLight(0xFFFFFF, 1, 10, 1);
-    const dir = new SpotLight(0xFFFFFF, 0.8, 7, 0.8, 1, 1);
-    const ambi = new AmbientLight( 0x404040 , 0.66);
-    const hemi = new HemisphereLight( 0xffffbb, 0x080820, 1.15 )
+    const point = new PointLight(0xFF5733, 1, 10, 1); // Цвет изменен на оранжево-красный
+    const dir = new SpotLight(0xFF5733, 0.8, 7, 0.8, 1, 1); // Цвет изменен на зеленый
+    const ambi = new AmbientLight(0xFF5733, 0.66); // Цвет изменен на серый
+    const hemi = new HemisphereLight(0xFF5733, 0x003366, 1.15); // Верхний цвет изменен на синий, нижний - на темно-синий
+
 
     dir.position.set(5, 1, 2);
     dir.target.position.set(0,0,0);
